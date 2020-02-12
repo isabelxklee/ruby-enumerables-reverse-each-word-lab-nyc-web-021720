49ff95f5e8c0f31b39e27bd247b8c7dac7b814a0
isabel k. lee <hello@isabelklee.co>
2020-02-12 18:33:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -12,9 +12,3 @@ def reverse_each_word(string)
     array.collect { |word| word.reverse }
     array.join(" ")
 end 
-
-def reverse_each(string)
-    array = string.split(" ")
-    array.collect { |word| word.reverse }
-    array.join(" ")
-end 
