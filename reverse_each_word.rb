e75cd955fce41537734c383525cbb7ac838fbce3
isabel k. lee <hello@isabelklee.co>
2020-02-12 18:36:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,6 +8,13 @@ def reverse_each_word(string)
     reversed = []
     array.each { |word| reversed.push(word.reverse) }
     reversed.join(" ")
+    end 
+
+def reverse_each_word(string)
+    array = string.split(" ")
+    reversed = []
+    array.each { |word| reversed.push(word.reverse) }
+    reversed.join(" ")
     
     # array.collect { |word| word.reverse }
     # array.join(" ")
